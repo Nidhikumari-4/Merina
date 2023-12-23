e9c50bec65abce1685454043f2f81f7832e07305
Nidhi Kumari <kumarinidhimain@gmail.com>
2023-12-24 03:16:02 +0530

    +

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Dashboard from "./pages/dashboard/index";
-import Login from "./pages/login/index";
+// import Login from "./pages/login/index";
 
 export default function Home() {
   return (
